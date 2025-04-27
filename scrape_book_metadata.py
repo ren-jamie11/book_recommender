69bf0e0e311f80a6990082e8aacb913e116fb424
ren-jamie11 <ren.jamie11@gmail.com>
2025-04-27 11:20:52 +0100

    added get title/author

--- scrape_book_metadata.py
+++ scrape_book_metadata.py
@@ -26,7 +26,7 @@ def regex_match(pattern, text):
 class BookMetaData:
     def __init__(self, url):
         self.url = url
-        self.main_content = None
+        self.html_content = None
         
         # basic info
         self.title = None 
@@ -39,22 +39,41 @@ class BookMetaData:
         self.overall_ratings = dict()
         self.ratings_histogram = dict()
 
-    def get_html_from_url(self, url):
-        try: 
-            source = requests.get(url)
-            source.raise_for_status() 
+    def get_html_from_url(self):
+        try:
+            source = requests.get(self.url)
+            source.raise_for_status()
         except Exception as e:
-            raise RequestFailedException(f"Failed to fetch URL: {url}. Reason: {e}")
+            raise RequestFailedException(f"Failed to fetch URL: {self.url}")
         
-        source_text = source.text
-        soup = BeautifulSoup(source_text, "lxml") 
+        soup = BeautifulSoup(source.text, "lxml")
+        html_content = soup.find('div', class_='BookPage__mainContent')
         
-        main_content = soup.find('div', class_='BookPage__mainContent')
-        
-        if main_content:
-            return main_content
+        if html_content:
+            return html_content
 
-        raise SoupNotFoundException(f"Could not find metadata html content for: {url}")
+        raise SoupNotFoundException(f"Could not find metadata html content for: {self.url}")
         
+    def get_title(self):
+        try:
+            content = self.html_content
+            title = content.find('h1', class_ ="Text Text__title1").text
+        except Exception as e:
+            raise SoupNotFoundException(f"Could not find title from html content ({e})")
+        
+        return title
+    
+    def get_author(self):
+        try:
+            content = self.html_content
+            author_div = content.find('div', class_="ContributorLinksList")
+            author = author_div.find('a', class_ = 'ContributorLink').text
+
+        except Exception as e:
+            raise SoupNotFoundException(f"Could not find author from html content ({e})")
+        
+        return author
+    
     
 
+        
