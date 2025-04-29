9ac0a0a376c17be4697d5fa6951bd9002890522b
ren-jamie11 <ren.jamie11@gmail.com>
2025-04-29 14:29:04 +0100

    big aggregate

--- process_parquets.py
+++ process_parquets.py
@@ -10,16 +10,16 @@ import pandas as pd
 import glob
 import os
 
-def combine_files(suffix, output_filename, file_type='parquet', input_dir='user_scraper_results'):
+def combine_files(suffix, output_filename, file_type='parquet', input_dir=''):
     parquet_files = glob.glob(os.path.join(input_dir, f'*_{suffix}.{file_type}'))
     
     dfs = []
     for file in parquet_files:
         df = pd.read_parquet(file, engine='pyarrow')
 
-        # # Detect columns of dtype datetime64 and cast them to string
-        # for col in df.select_dtypes(include=['datetime64[ns]', 'datetime64[ns, UTC]']).columns:
-        #     df[col] = df[col].astype(str)
+        # Detect columns of dtype datetime64 and cast them to string
+        for col in df.select_dtypes(include=['datetime64[ns]', 'datetime64[ns, UTC]']).columns:
+            df[col] = df[col].astype(str)
 
         dfs.append(df)
     
@@ -29,8 +29,8 @@ def combine_files(suffix, output_filename, file_type='parquet', input_dir='user_
     print(f'Combined {len(parquet_files)} files into {output_filename}')
 
 
-original_users_filename = 'user_scraper_results/users_data.parquet'
-user_reviews_filename = 'user_scraper_results/users_40_book_reviews.parquet'
+original_users_filename = 'final_aggregated_results/new_books.parquet'
+user_reviews_filename = 'final_aggregated_results/user_new_book_reviews.parquet'
 
-combine_files(suffix = 'data', output_filename = original_users_filename)
-combine_files(suffix = 'reviews', output_filename = user_reviews_filename)
+combine_files(suffix = 'data', output_filename = original_users_filename, input_dir = 'new_titles_scraper_results')
+combine_files(suffix = 'reviews', output_filename = user_reviews_filename, input_dir = 'new_titles_scraper_results')
